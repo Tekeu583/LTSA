b77e28b035cd2bb49b5e3bab40b779705798b9dc
BOUM <albertoboum86@gmail.com>
2025-03-29 16:09:48 +0100

    ajout de la page Formations

---
+++ views/formations.php
@@ -0,0 +1,50 @@
+<?php 
+ob_start();
+?>
+
+    <!-- Titre de la page --> 
+
+    <!-- Description des formations du LTSA -->
+
+
+    <!-- Titre de la section des parcours d'etude-->
+
+    <h3>Parcours d’etude pour les étudiants de master et Doctorat/PhD</h3><br>
+
+
+    <h3>Année Académique 2024/2025</h3><br>
+
+    <h3>Cycle de Master 2 Recherche</h3><br>
+    <!-- Parcours ENERGETIQUE -->
+    <!-- Parcours GENIE CIVIL -->
+    <!-- Parcours GENIE MECANIQUE -->
+    <!-- Parcours ELECTRONIQUE, ELECTROTECHNIQUE, AUTOMATIQUE ET TELECOMMUNICATION -->
+
+    <h3>Cycle de Doctorat</h3><br>
+    <!-- PREMIERE ANNEE DE DOCTORAT -->
+
+    <!-- DEUXIEME ANNEE DE DOCTORAT -->
+
+    <!-- TROISIEME ANNEE DE DOCTORAT -->
+
+
+    <!-- Description des années de cycle de Doctorat -->
+
+    <!-- LTSA Admission -->
+     
+<?php
+$content=ob_get_clean();
+require("template.php");
+?>
+
+
+
+     
+
+
+
+
+
+     
+
+
